2f99bd83f4a210f4e5ec7dc4015d302b9d77e902
mkvaternyuk <casya86@mail.ru>
2017-08-19 14:51:57 +0300

    Customize sass

--- diary/src/App.js
+++ diary/src/App.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+class Day extends Component {
+    render() {
+        return (
+          <div>Day Box</div>
+        );
+    }
+}
+
 class App extends Component {
   render() {
     return (
@@ -13,6 +22,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+          <Day />
       </div>
     );
   }
